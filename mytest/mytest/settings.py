21df2c156ce7c0aa00986a7c45c032754121a61e
vishaldubey <vkd2695@gmail.com>
2025-09-26 12:06:48 +0530

    chnges for settings db

--- mytest/mytest/settings.py
+++ mytest/mytest/settings.py
@@ -74,12 +74,26 @@ WSGI_APPLICATION = 'mytest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+        'USER': 'vishal',
+        'PASSWORD': 'StrongPassword123!@#',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
