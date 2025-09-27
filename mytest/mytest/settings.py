f3bc8cd72bceb57a1af8a29da8b02319ef0d7f39
vishaldubey <vkd2695@gmail.com>
2025-09-27 16:18:15 +0530

    chnges

--- mytest/mytest/settings.py
+++ mytest/mytest/settings.py
@@ -131,12 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = '/home/ubuntu/project/testrepo/mytest/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # MEDIA files (user uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/ubuntu/project/testrepo/mytest/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
